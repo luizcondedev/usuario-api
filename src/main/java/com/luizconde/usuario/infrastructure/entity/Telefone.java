330aac5561d7db9015adc0118d869f4b201b29dd
Luiz Conde <luizconde.deev@gmail.com>
2026-07-06 15:57:47 -0300

    Adicionado metodo que salva dados usuario

--- src/main/java/com/luizconde/usuario/infrastructure/entity/Telefone.java
+++ src/main/java/com/luizconde/usuario/infrastructure/entity/Telefone.java
@@ -1,16 +1,14 @@
 package com.luizconde.usuario.infrastructure.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "telefones")
 public class Telefone {
     @Id
